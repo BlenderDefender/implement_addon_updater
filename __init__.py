3a1d56d166c160de55f5158c36a743d6b3ad1638
Blender Defender <defenderblender@gmail.com>
2021-02-10 21:18:42 +0100

    Remove Updater from the addon, because the .blend file (Most important file) doesn't get updated.

--- __init__.py
+++ __init__.py
@@ -38,7 +38,6 @@ from os.path import expanduser
 
 import shutil
 import platform
-from . import addon_updater_ops
 
 # Multiple uses:
 # -----------------------------------------------------------------------------
@@ -206,63 +205,10 @@ def menu_func(self, context):
 class IMPLEMENTUPDATER_APT_Preferences(bpy.types.AddonPreferences):
     bl_idname = __package__
 
-    # addon updater preferences
-
-    auto_check_update = bpy.props.BoolProperty(
-        name="Auto-check for Update",
-        description="If enabled, auto-check for updates using an interval",
-        default=True,
-        )
-    updater_intrval_months = bpy.props.IntProperty(
-        name='Months',
-        description="Number of months between checking for updates",
-        default=0,
-        min=0
-        )
-    updater_intrval_days = bpy.props.IntProperty(
-        name='Days',
-        description="Number of days between checking for updates",
-        default=7,
-        min=0,
-        max=31
-        )
-    updater_intrval_hours = bpy.props.IntProperty(
-        name='Hours',
-        description="Number of hours between checking for updates",
-        default=0,
-        min=0,
-        max=23
-        )
-    updater_intrval_minutes = bpy.props.IntProperty(
-        name='Minutes',
-        description="Number of minutes between checking for updates",
-        default=0,
-        min=0,
-        max=59
-        )
-
     def draw(self, context):
         layout = self.layout
-        # col = layout.column() # works best if a column, or even just self.layout
-        mainrow = layout.row()
-        col = mainrow.column()
         layout.operator("wm.url_open", text="Checkout Gumroad for other addons and more...", icon='FUND').url="https://gumroad.com/blenderdefender"
 
-
-        # updater draw function
-        # could also pass in col as third arg
-        addon_updater_ops.update_settings_ui(self, context)
-
-        # Alternate draw function, which is more condensed and can be
-        # placed within an existing draw function. Only contains:
-        #   1) check for update/update now buttons
-        #   2) toggle for auto-check (interval will be equal to what is set above)
-        # addon_updater_ops.update_settings_ui_condensed(self, context, col)
-
-        # Adding another column to help show the above condensed ui as one column
-        # col = mainrow.column()
-        # col.scale_y = 2
-        # col.operator("wm.url_open","Open webpage ").url=addon_updater_ops.updater.website
 
 
 classes = (
@@ -271,25 +217,14 @@ classes = (
 )
 
 def register():
-    # addon updater code and configurations
-    # in case of broken version, try to register the updater first
-    # so that users can revert back to a working version
-    addon_updater_ops.register(bl_info)
-    
-    bpy.types.TEXT_MT_text.append(menu_func) #put in register()
+    bpy.types.TEXT_MT_text.append(menu_func)
 
-    # register the example panel, to show updater buttons
     for cls in classes:
-        addon_updater_ops.make_annotations(cls) # to avoid blender 2.8 warnings
         bpy.utils.register_class(cls)
 
 
 def unregister():
-    # addon updater unregister
-    addon_updater_ops.unregister()
+    bpy.types.TEXT_MT_text.remove(menu_func)
 
-    bpy.types.TEXT_MT_text.remove(menu_func) #put in register()
-    # register the example panel, to show updater buttons
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
-
